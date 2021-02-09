a198570a3f2e420f9207bdb5bc06bfca82d97cc3
Morrism1 <morrismontana0@gmail.com>
2021-02-09 16:55:44 +0200

    Create a module for project creation

--- src/addproject.js
+++ src/addproject.js
@@ -1,34 +1,9 @@
-const projects = document.querySelector('data-lists');
-
-lists = [
-    {
-        id: 1,
-        name: 'project1'
-    },
-    {
-        id: 2,
-        name: 'project1'
-    }
-]
-
-function renderP()
-{ clearList(projects);
-    lists.foreach(list =>
-        {
-            const project= document.createElement('li');
-            project.classList.add="list-item";
-            project.dataset.listId=list.id;
-            project.innerText=list.name;
-            project.appendChild(project)
-        })
+function createProject(name) {
+  return {
+    id: Date.now().toString(),
+    name: name,
+    tasks: [],
+  };
 }
 
-function clearList(list)
-{
-    while(list.firstChild)
-    {
-        list.remove(list.firstChild);
-    }
-}
-
-renderP();
+export default createProject;
